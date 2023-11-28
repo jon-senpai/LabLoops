d49231378578422c40fa21e3257cdfa39962e5e3
Jon L <lauturejonathan9@gamil.com>
2023-11-28 16:18:24 -0500

    Part_one

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-/*Loop through all numbers from 1 to 100.
-If a number is divisible by 3, log “Fizz.”
-If a number is divisible by 5, log “Buzz.”
-If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-If a number is not divisible by either 3 or 5, log the number./*
+//Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.//
+let x = 1 
+while (x < 101){
+    if (x % 3 === 0){
+        console.log(x + 'Fizz')}
+        if(x % 5 === 0){
+         console.log(x + 'Buzz')}
+        if (x % 3 === 0 && x % 5 === 0){
+            console.log('Fizz Buzz')}
+            if (x % 3 !==0 && x % 5 !==0){
+                console.log(x)}
+          
+
+
+x++
+}
