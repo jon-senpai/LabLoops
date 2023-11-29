6aac3e7b7edcdf81830ed71036a3ed42d513e898
Jon L <lauturejonathan9@gamil.com>
2023-11-29 14:54:38 -0500

    Part two

--- index.js
+++ index.js
@@ -3,18 +3,35 @@
 // If a number is divisible by 5, log “Buzz.”
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.//
-let x = 1 
-while (x < 101){
-    if (x % 3 === 0){
-        console.log(x + 'Fizz')}
-        if(x % 5 === 0){
-         console.log(x + 'Buzz')}
-        if (x % 3 === 0 && x % 5 === 0){
-            console.log('Fizz Buzz')}
-            if (x % 3 !==0 && x % 5 !==0){
-                console.log(x)}
+// let x = 1 
+// while (x < 101){
+//     if (x % 3 === 0){
+//         console.log(x + 'Fizz')}
+//         if(x % 5 === 0){
+//          console.log(x + 'Buzz')}
+//         if (x % 3 === 0 && x % 5 === 0){
+//             console.log('Fizz Buzz')}
+//             if (x % 3 !==0 && x % 5 !==0){
+//                 console.log(x)}
           
 
 
-x++
+// x++
+// }
+let n = 3
+while (n < 51)
+{
+        let Prime = true;
+        let divisor = 2; 
+            while (divisor <= Math.sqrt(n)){
+                if (n % divisor === 0){
+                    Prime = false;
+                    break;
+                }
+                divisor++;
+            }
+                if (Prime){
+                    console.log(n + "Prime");
+                }
+                n++;
 }
