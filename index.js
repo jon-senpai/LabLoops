09434a3ad3baa64ba1da62af42dd077a508d978f
Jon L <lauturejonathan9@gamil.com>
2023-11-29 21:18:49 -0500

    FinishedFinished

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-//Loop through all numbers from 1 to 100.
-// If a number is divisible by 3, log “Fizz.”
-// If a number is divisible by 5, log “Buzz.”
-// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
-// If a number is not divisible by either 3 or 5, log the number.//
-// let x = 1 
-// while (x < 101){
-//     if (x % 3 === 0){
-//         console.log(x + 'Fizz')}
-//         if(x % 5 === 0){
-//          console.log(x + 'Buzz')}
-//         if (x % 3 === 0 && x % 5 === 0){
-//             console.log('Fizz Buzz')}
-//             if (x % 3 !==0 && x % 5 !==0){
-//                 console.log(x)}
+Loop through all numbers from 1 to 100.
+If a number is divisible by 3, log “Fizz.”
+If a number is divisible by 5, log “Buzz.”
+If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+If a number is not divisible by either 3 or 5, log the number.//
+let x = 1 
+while (x < 101){
+    if (x % 3 === 0){
+        console.log(x + 'Fizz')}
+        if(x % 5 === 0){
+         console.log(x + 'Buzz')}
+        if (x % 3 === 0 && x % 5 === 0){
+            console.log('Fizz Buzz')}
+            if (x % 3 !==0 && x % 5 !==0){
+                console.log(x)}
           
 
 
-// x++
-// }
-// let n = 3
-// while (n < 51)
-// {
-//         let Prime = true;
-//         let divisor = 2; 
-//             while (divisor <= Math.sqrt(n)){
-//                 if (n % divisor === 0){
-//                     Prime = false;
-//                     break;
-//                 }
-//                 divisor++;
-//             }
-//                 if (Prime){
-//                     console.log(n + "Prime");
-//                 }
-//                 n++;
-// }
-// let id1 = 42;
-// let name1 = "Bruce";
-// let occupation1 = "Knight";
-// let age1 = "41";
+x++
+}
+let n = 3
+while (n < 51)
+{
+        let Prime = true;
+        let divisor = 2; 
+            while (divisor <= Math.sqrt(n)){
+                if (n % divisor === 0){
+                    Prime = false;
+                    break;
+                }
+                divisor++;
+            }
+                if (Prime){
+                    console.log(n + "Prime");
+                }
+                n++;
+}
+let id1 = 42;
+let name1 = "Bruce";
+let occupation1 = "Knight";
+let age1 = "41";
 const csv = [
     { ID: 42, Name: "Bruce", Occupation: "Knight", Age: 41 },
     { ID: 57, Name: "Bob", Occupation: "Fry Cook", Age: 19 },
@@ -50,5 +50,5 @@ const csv = [
     for (const key in person) {
       console.log(`${key}: ${person[key]}`);
     }
-    console.log('\n'); // Separate each person's information with a new line
+    console.log('\n'); 
   }
